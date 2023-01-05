5b389c5960e77acf13e507dd743ea04c516e6ccc
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2023-01-06 00:01:21 +0530

    ::ign::

--- example/server.js
+++ example/server.js
@@ -1,8 +1,14 @@
 const Router = require('../index.js');
 
 const router = new Router(3000);
-router.SETTINGS.CLEAR_CACHE_TIMING = 20; // seconds
+router.SETTINGS.CLEAR_CACHE_TIMING = 0; // seconds
 
-router.start({
-    name: 'hey there' // referenced in "script.js"(line:2) and "home.html" (line:18)
+router.variable = {
+    name: 'this will be changed' // referenced in "script.js"(line:2) and "home.html" (line:18)
+};
+
+router.start();
+
+router.on('get', (req, res) => {
+    router.variable.name = 'SS-Router'
 });
